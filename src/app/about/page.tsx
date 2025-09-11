f656c51b2b42068f9cf3bf16c399469ccdeb8119
miyashitaSora <sora.miyashita@rensa.co.jp>
2025-09-11 10:28:03 +0900

    feat(aboutページ追加/Navbarスタイル調整)

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import style from './about.module.scss'
+
+export default function About(){
+    return(
+        <>
+        <h1>こちらは説明ページです</h1>
+        <p>Next.jsの学習中。このページは改修予定です</p>
+        </>
+    )
+}
